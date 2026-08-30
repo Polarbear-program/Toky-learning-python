339718225bf3513fb45c5e9046de7a900b6fa043
Toky <tokybear2003@gmail.com>
2026-08-30 16:39:29 +0700

    Updatge count_hi

--- problems/Strings/count_hi.py
+++ problems/Strings/count_hi.py
@@ -1,24 +1,27 @@
-# Return the number of times that the string "hi" appears anywhere 
+# Return the number of times that the string "hi" appears anywhere
 # in the given string.
-#____________________________________________#
+# ____________________________________________#
 
 # count_hi('abc hi ho') → 1
 # count_hi('ABChi hi') → 2
 # count_hi('hihi') → 2
 
 def count_hi(str):
-  count = 0
-  for search in range(len(str)-1):
-    if str[search] == "h" and str[search + 1] == "i":
-      count +=1
-  return count
+    count = 0
+    for search in range(len(str)-1):
+        if str[search] == "h" and str[search + 1] == "i":
+            count += 1
+    return count
+
 
 def count_hi_sh(str):
-  count = 0
-  for search in range(len(str)-1):
-    if str[search:search+2] == "hi":
-      count +=1
-  return count
+    count = 0
+    for search in range(len(str)-1):
+        if str[search:search+2] == "hi":
+            count += 1
+    return count
+
 
 string = "Rodhisia"
 print(count_hi_sh(string))
+print(string[0:2])
