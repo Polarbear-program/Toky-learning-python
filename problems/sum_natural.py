6d20c1981297ff70f5c43399ca189221eac7cd65
Toky <tokybear2003@gmail.com>
2026-08-30 08:54:39 +0700

    Sum of natural correction

--- problems/sum_natural.py
+++ problems/sum_natural.py
@@ -1,14 +1,17 @@
 def sum_of_natural(number_input: int):
+    if number_input == 1:
+        return 1
+    
     for i in range(1, number_input):
 
         # Number input will summarize from 1 to itself
-        result = num_input + i
+        number_input += i
         print(i, "+", end=" ")
         i += 1
 
     # After the loop end, close it with the last index of element and "="
     print(i, "= ", end="")
-    return result
+    return number_input
 
 
 print("Please enter any positive integer:", end="")
