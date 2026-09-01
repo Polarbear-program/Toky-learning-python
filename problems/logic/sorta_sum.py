5c22d9b8b7828aba1ed68922bcb22b7de7d2c1ad
Toky <tokybear2003@gmail.com>
2026-09-01 10:43:24 +0700

    Add sorta_sum.py

---
+++ problems/logic/sorta_sum.py
@@ -0,0 +1,10 @@
+def sorta_sum(a, b):
+  if 10 <= (a + b) < 20:
+    return 20
+  else:
+    return a + b
+
+print(sorta_sum(2,6))
+print(sorta_sum(5, 7))
+print(sorta_sum(5,5))
+print(sorta_sum(10,23))
